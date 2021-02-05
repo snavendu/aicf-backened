2975e0af404a5a602b5caf2d97bae399b841073c
Aman Jakhar <gitaman-design>
2021-02-05 14:21:29 +0530

    App

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Form, Input, Select, Row, Col, Checkbox, Button, Upload, message, Radio } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import React  from 'react'
+import { Form, Input, Select, Row, Col, Checkbox, Button, Upload, message, Radio, Layout, Menu, Breadcrumb, DatePicker } from 'antd';
+import { UploadOutlined, HomeOutlined, SearchOutlined, DollarCircleOutlined, FileTextOutlined } from '@ant-design/icons';
 
+
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const { TextArea } = Input;
 
-const areas = [
-    { label: 'Beijing', value: 'Beijing' },
-    { label: 'Shanghai', value: 'Shanghai' },
+const gender = [
+    { label: 'Male', value: 'Male' },
+    { label: 'Female', value: 'Female' },
+];
+
+const relation = [
+    { label: 'Daughter', value: 'Daugher' },
+    { label: 'Son', value: 'Son' },
+    { label: 'Husband', value: 'Husband' },
+    { label: 'Son', value: 'Son' },
+    { label: 'Brother', value: 'Brother' },
+    { label: 'Sister', value: 'Sister' },
 ];
 
 const props = {
@@ -32,18 +45,22 @@ function onChange(e) {
     console.log(`checked = ${e.target.checked}`);
   }
   
-
+  function onChangeDate(date, dateString) {
+    console.log(date, dateString);
+  }
 
 
 const renderForm = () => {
     return (
         <>
+            {/* Row number 1 */}
+
             <Row gutter={[30, 20]}>
                 <Col span={8}>   
                     
                 <Form.Item
-                    name="Full Name"
-                    label="Full Name"
+                    name="First Name"
+                    label="First Name"
                     rules={[
                 {
                     type: "text",
@@ -51,24 +68,49 @@ const renderForm = () => {
                 
                 {
                     required: true,
-                    message: 'Please input your E-mail!',
+                    message: 'Please input your First Name!',
                 },
                 ]}
             >
                 <Input />
                     </Form.Item>
                 </Col>
+
+                <Col span={8}>   
+                    
+                <Form.Item
+                    name="Middle Name"
+                    label="Middle Name"
+                    rules={[
+                {
+                    type: "text",
+                },
+                
+                ]}
+            >
+                <Input />
+                    </Form.Item>
+                </Col>
+
                 <Col span={8}>
                     
                 <Form.Item
-                    name="Telephone/Fax"
-                    label="Telephone/Fax"
+                    name="Last name"
+                    label="Last Name"
             >
                 <Input />
                     </Form.Item>
 
                 </Col>
-                <Col span={8}>
+
+                </Row>
+
+            
+            {/* Row number 2 */}
+
+            <Row gutter={[30, 20]}>
+                
+            <Col span={12}>
                 <Form.Item
                     name="Email"
                     label="Email"
@@ -85,105 +127,119 @@ const renderForm = () => {
             >
                 <Input />
                     </Form.Item>
-                </Col>                
-            </Row>
-            <Row gutter={[30, 20]}>
-                
-                <Col span={8}>
-                    <Form.Item name="State" label="State" rules={[{ required: true, message: 'Missing State' }]}>
-                        <Select options={areas} />
-                    </Form.Item>
+                </Col>
+                <Col span={12}>
+                    <Form.Item
+                        name="MobileNumber"
+                        label="Mobile Number"
+                        rules={[
+                    {
+                        type: "text",
+                    },
+                    
+                    {
+                        required: true,
+                        message: 'Please input your Mobile Number',
+                    },
+                    ]}
+                >
+                    <Input />
+                        </Form.Item>
                 </Col>
 
-                <Col>
+                </Row>
+                                
+            
+            {/* Row number 3 */}
+
+            <Row gutter={[30, 20]}>
+
+            <Col span={8}>
                 <Form.Item
-                    name="Son/Daughter"
-                    label="Son/Daughter Of"
+                    name="SonDaughterOf"
+                    label="Son/Daugher of"
                     rules={[
                         {
                         required: true,
                         message: 'Please add your fathers name!',
                         },
-                    ]}
-                    hasFeedback
-                    >
+                    ]}>
                     <Input />
                 </Form.Item>
                 </Col>
+
+                
                 <Col span={8}>
-                    <Form.Item name="Relationship" label="Relationship" rules={[{ required: true, message: 'Add relationship' }]}>
-                        <Select options={areas} />
+                    <Form.Item name="Relationship" label="Relationship" rules={[{ required: true, message: 'Select Relationship' }]}>
+                        <Select options={relation} />
                     </Form.Item>
+                </Col>
+
+                <Col span={8}>
+                <Form.Item
+                    name="MotherTounge"
+                    label="Mother Tounge">
+                    <Input />
+                </Form.Item>
                 </Col>
             </Row>
             
-
-            <Row gutter={[ 30, 20]}>
+            {/* Row number 4 */}
+            
+            <Row gutter={[30, 40]}>
                 
-                <Col span={6}>
-                    <Form.Item name="Gender" label="Gender" rules={[{ required: true, message: 'Select Gender' }]}>
-                        <Select options={areas} />
+            
+
+                
+                <Col span={8}>
+                    <Form.Item name="Gender" label="Gender" rules={[{ required: true, message: 'Add Gender' }]}>
+                        <Select options={gender} />
                     </Form.Item>
                 </Col>
-
-                <Col span={6}>
-                <Form.Item
-                    name="Mother-tounge"
-                    label="Mother Tounge"
-                    >
-                    <Input />
-                </Form.Item>
-                </Col>
-                <Col span={6}>
-                <Form.Item
-                    name="Mobile Phone"
-                    label="Mobile Phone"
-                    >
-                    <Input />
-                </Form.Item>
-                </Col>
-                <Col span={6}>
+                
+                <Col span={12} offset={2}>
                 <Form.Item
                     name="Date of Birth"
                     label="Date of Birth"
                     >
-                    <Input />
+                    <DatePicker onChange={onChangeDate} />
+
                 </Form.Item>
                 </Col>
                 
             </Row>
 
+             {/* Row number 5 */}
+
             <Row gutter={[ 30, 20]}>
-                <Col span={12}>
+                <Col span={6}>
                     <Form.Item
                         name="Address"
                         label="Address">
                         <TextArea rows={2} />
                     </Form.Item>
                 </Col>
-            </Row>
-
-            <Row style={{ marginTop: 20 }}>
-                <Col span={10}>
-                    <Upload {...props}>
-                        <Button type="default" icon={<UploadOutlined />}>Passport Size Photo</Button>
-                    </Upload>
-                    
-                    <h4 style={{marginTop: 20}}>
-                        (jpg, png or gif images only. 160x200 pixels (width x height). Maximum size 1000 KB)
-                        </h4>
-
+                <Col span={6}>
+                    <Form.Item name="city" label="City" rules={[{ required: true, message: 'Add City' }]}>
+                        <Input />
+                    </Form.Item>
                 </Col>
-                <Col span={10} offset={4}>
-                    <Upload {...props}>
-                        <Button type="default" icon={<UploadOutlined />}>Birth Certificate</Button>
-                    </Upload>
-                    <h4 style={{marginTop: 20}}>(Maximum size 1000 KB)</h4>
-
+                <Col span={6}>
+                    <Form.Item name="district" label="District" rules={[{ required: true, message: 'Add City' }]}>
+                        <Input />
+                    </Form.Item>
+                </Col>
+                <Col span={6}>
+                    <Form.Item name="state" label="State" rules={[{ required: true, message: 'Add City' }]}>
+                        <Input />
+                    </Form.Item>
                 </Col>
             </Row>
 
-            <Row gutter={[ 30,20]} style={{ marginTop: 20, marginBottom: 20}}>
+            {/* Row number 6 */}
+            
+
+            <Row gutter={[ 30,20]} style={{ marginTop: 10, marginBottom: 20}}>
                 <Col span={8}>
                     <h3>Select Type</h3>
                     <Checkbox onChange={onChange}>Player</Checkbox>
@@ -196,9 +252,35 @@ const renderForm = () => {
                     <Radio>No</Radio>
                 </Col>
             </Row>
+
+
+            
+            
+             {/* Row number 7 */}
+
+
+             <Row style={{marginTop: '30px'}}>
+                <Col span={8}>
+                    <Upload {...props}>
+                        <Button type="default" icon={<UploadOutlined />}>Passport Size Photo</Button>
+                    </Upload>
+                    
+                    <p style={{marginTop: 20}}>
+                        (jpg, png or gif images only. 160x200 pixels (width x height). Maximum size 1000 KB)
+                        </p>
+
+                </Col>
+                <Col span={8} offset={4}>
+                    <Upload {...props}>
+                        <Button type="default" icon={<UploadOutlined />}>Birth Certificate</Button>
+                    </Upload>
+                    <p style={{marginTop: 20}}>(Maximum size 1000 KB)</p>
+
+                </Col>
+            </Row>
             
 
-            <Row justify="end">
+            <Row>
                 <Col>
                 
         <Form.Item >
@@ -214,12 +296,27 @@ const renderForm = () => {
 }
   
 
+
+
     
 
 
 
 
 export default function FormPRS() {
+
+    
+    
+ //   const { collapsed } = this.state;
+
+   // const state = {
+     //   collapsed: false,
+      //};
+    
+   // const  onCollapse = collapsed => {
+     //   console.log(collapsed);
+       // this.setState({ collapsed });
+     // };
 
     
 
@@ -233,36 +330,67 @@ export default function FormPRS() {
 
 
     return (
-        <div style={{ margin: '40px' }}>
-            
-            <div style={{ marginBottom: 40}}>
+        <>
 
-            <h2>
-                All India Chess Federation
-        </h2>
-            
-            <p>
-                    For existing players please go to your player page and click on extend membership
-            </p>
+            <Layout style={{ minHeight: '100vh' }}>
                 
-            </div>
+            <Sider style={{ paddingTop: 30}}>
+                <div className="logo"  />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1" icon={<HomeOutlined />}>
+                    AICF Home
+                </Menu.Item>
+                <Menu.Item key="2" icon={<SearchOutlined />}>
+                    Search
+                </Menu.Item>
+                <Menu.Item key="3" icon={<FileTextOutlined />}>
+                            Register New
+                </Menu.Item>
+                <Menu.Item key="4" icon={<DollarCircleOutlined />}>
+                    Renew Membership
+                </Menu.Item>
+                </Menu>
+                </Sider>
+                <Layout className="site-layout" style={{backgroundColor: "#ffffff"}}>
 
+                    <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>AICF PRS</Breadcrumb.Item>
+                        <Breadcrumb.Item>New Registration</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ marginLeft: '20px', marginTop: '20px', marginRight: '20px' }}>
             
+                        <div style={{ marginBottom: 40}}>
 
-        <Form
-        form={form}
-        name="register"
-        onFinish={onFinish}
-        scrollToFirstError
-      >
+                        <h2>
+                            All India Chess Federation
+                        </h2>
+            
+                        <p>
+                            For existing players please go to your player page and click on extend membership
+                        </p>
+                
+                    </div>
+                        <Form
+                            form={form}
+                            name="register"
+                            onFinish={onFinish}
+                            scrollToFirstError
+                        >
         
   
-        {renderForm()}
+                        {renderForm()}
   
         
-      </Form>
+                        </Form>
             
-        </div>
+                    </div>
+                </Content>
+                    {/*         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+        </Layout>
+        
+        </Layout>
+        </>
     )
 }
 
