2621b8b6e7dd671c988b0f7e395dad25099f9887
Rajnish Singh <rajnish42413@gmail.com>
2021-02-12 14:36:49 +0530

    make it better

--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
-    key: process.env.APP_KEY,
-    port: process.env.APP_PORT || 5000,
-    host: process.env.APP_HOST || "127.0.0.1"
+    key: process.env.APP_KEY || 'djfkdjfkdk',
+    port: process.env.APP_PORT || 80,
+    host: process.env.APP_HOST || "0.0.0.0"
 };
