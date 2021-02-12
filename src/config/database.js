1eafa7619564967d41cc32e4552ef8d7c2c8f2c4
Rajnish Singh <rajnish42413@gmail.com>
2021-02-12 13:00:17 +0530

    remove .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || "3306",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: "bots.cszx2vxjkisf.ap-south-1.rds.amazonaws.com",
+    port: "3306",
+    user: "aicfprs",
+    password: ">t/vP&=,9]L)/54+",
+    database: "prs_aicf"
 };
