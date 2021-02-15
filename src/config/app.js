e5fa73c777d861fd9dfa52c1f49b143d225d8773
Rajnish Singh <rajnish42413@gmail.com>
2021-02-15 19:25:28 +0530

    app setup for local

--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
     key: process.env.APP_KEY || 'djfkdjfkdk',
-    port: process.env.APP_PORT || 3000,
-    host: process.env.APP_HOST || "0.0.0.0"
+    port: process.env.APP_PORT || 3001,
+    host: process.env.APP_HOST || "127.0.0.1"
 };
