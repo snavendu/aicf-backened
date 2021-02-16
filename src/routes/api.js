aeec024d950c861f5ea6fd9975d02d5213387659
= <=>
2021-02-16 14:04:00 +0530

    done

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,5 @@
 const { Router } = require("express"),
-    bodyParser = require('body-parser'),
+    bodyParser = require("body-parser"),
     authMiddleware = require("../middlewares/authentication");
 const controller = require("../controllers");
 
@@ -10,11 +10,9 @@ const userController = controller("userController"),
 const route = Router();
 route.use(bodyParser.json());
 route.post("/login", loginController.login);
-route.post('/register', registerController.register);
+route.post("/register", registerController.register);
 route.get("/users", userController.index);
 route.get("/contacts", userController.index);
 
-
 route.use(authMiddleware());
-
 module.exports = route;
