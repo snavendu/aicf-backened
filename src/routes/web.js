2280f623d3c8d5ac6c9bd736f0c24dacb3dcd161
= <=>
2021-02-16 14:18:03 +0530

    done

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,6 +3,7 @@ const { Router } = require("express"),
     homeController = controller("homeController");
 
 const app = Router();
+
 app.get("/", homeController.index);
 app.get("/different-code", homeController.diffCode);
 module.exports = app;
